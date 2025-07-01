15c88204e893e3d04143b9bcc99cf3d988a04c53
TomiiYurina <yurina@tomii14.local>
2025-07-02 00:25:31 +0900

    仮のデータを返す

--- server/app/api/minor_sports_api.py
+++ server/app/api/minor_sports_api.py
@@ -2,6 +2,34 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("/message")
-async def get_message():
-    return {"text": "Hello, World!"}
+@router.get("/minor-sports")
+async def get_minor_sports():
+    return [
+        {
+            "id": 1, 
+            "name": "野球",
+            "min-participants": 9,
+            "max-participants": 18,
+            "tool": "バット、ボール、グローブ",
+            "budget": 50000,
+            "place": "野球場",
+        },
+        {
+            "id": 2, 
+            "name": "サッカー",
+            "min-participants": 11,
+            "max-participants": 22,
+            "tool": "ボール、シューズ、ユニフォーム",
+            "budget": 60000,
+            "place": "サッカー場",
+        },
+        {
+            "id": 3, 
+            "name": "バスケットボール",
+            "min-participants": 5,
+            "max-participants": 10,
+            "tool": "ボール、シューズ",
+            "budget": 30000,
+            "place": "体育館",
+        },
+    ]
