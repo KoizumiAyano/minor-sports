3364cff54e1a30d761f18537f37bfd296ab9f046
TomiiYurina <yurina@tomii14.local>
2025-07-03 21:04:22 +0900

    データを返す

--- server/app/api/minor_sports_api.py
+++ server/app/api/minor_sports_api.py
@@ -7,29 +7,62 @@ async def get_minor_sports():
     return [
         {
             "id": 1, 
-            "name": "野球",
-            "min-participants": 9,
-            "max-participants": 18,
-            "tool": "バット、ボール、グローブ",
-            "budget": 50000,
-            "place": "野球場",
+            "name": "ベースボール5",
+            "min-participants": 5,
+            "max-participants": 10,
+            "tool": "ボール",
+            "budget": 700,
+            "place": "屋内",
+            "description": "ベースボール5は、野球の要素を取り入れた新しいスポーツで、5人制で行われます。"
         },
         {
             "id": 2, 
-            "name": "サッカー",
-            "min-participants": 11,
-            "max-participants": 22,
-            "tool": "ボール、シューズ、ユニフォーム",
-            "budget": 60000,
-            "place": "サッカー場",
+            "name": "モルック",
+            "min-participants": 2,
+            "max-participants": 6,
+            "tool": "モルック，スキットル",
+            "budget": 10000,
+            "place": "屋外",
+            "description": "モルックは、フィンランド発祥のスティックを使った投擲競技で、2人以上で楽しめます。"
         },
         {
             "id": 3, 
-            "name": "バスケットボール",
-            "min-participants": 5,
-            "max-participants": 10,
-            "tool": "ボール、シューズ",
-            "budget": 30000,
-            "place": "体育館",
+            "name": "ディスクゴルフ",
+            "min-participants": 2,
+            "max-participants": "制限なし",
+            "tool": "ディスクゴルフセット",
+            "budget": 2500,
+            "place": "屋外",
+            "description": "ディスクゴルフは、フリスビーを使ってゴールに入れるスポーツで、2人以上で楽しめます。"
         },
+        {
+            "id": 4, 
+            "name": "ボッチャ",
+            "min-participants": 2,
+            "max-participants": 6,
+            "tool": "ボッチャボール",
+            "budget": 20000,
+            "place": "屋内",
+            "description": "ボッチャは、イタリア発祥の球技で、2人以上で行うことができ、特に障害者スポーツとしても人気があります。"
+        }
+        {
+            "id": 5, 
+            "name": "カバディ",
+            "min-participants": 7,
+            "max-participants": 14,
+            "tool": "なし",
+            "budget": 0,
+            "place": "屋内",
+            "description": "カバディは、インド発祥の接触型スポーツで、7人以上で行われます。"
+        },
+        {
+            "id": 6, 
+            "name": "ドッジビー",
+            "min-participants": 13,
+            "max-participants": 26,
+            "tool": "ドッジビー",
+            "budget": 1500,
+            "place": "屋内",
+            "description": "ドッジビーは、フリスビーを使った新しい形のドッジボールで、13人以上で楽しめます。"
+        }
     ]
